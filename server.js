c8a25615d05ed08cd34a132f0b14888841459149
EVERTZ_MICROSYS\bwang <bwang@evertz.com>
2018-10-16 16:32:56 -0400

    socketIO support

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const game = require('./game');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.emit('registerID', game.register(socket.id));
+  io.emit('changeTurn', game.changeTurn());
+
+  socket.on('playerMove', (role, x, y) => {
+    io.emit('boardPlace', game.place(role, x, y));
+    game.toggleTurn();
+    io.emit('changeTurn', game.changeTurn());
+  });
+
+  socket.on('disconnect', () => {
+    game.unregister(socket.id);
+    io.emit('changeTurn', game.changeTurn());
+  });
+});
+
+http.listen(PORT, () => {
+  console.log('listening on *:' + PORT);
+});
